9798dd2440dce0355f35ffa15e200189093ee7c7
bbl-Laobu <bernardblanckaert@yahoo.co.uk>
2017-05-20 08:11:36 +0700

    InApp ReadMe Webpage added

--- SlideMenuDemo/Views/MyFirstView.xaml.cs
+++ SlideMenuDemo/Views/MyFirstView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using System.Diagnostics;
 using CarouselView.FormsPlugin.Abstractions;
+using SlideMenuDemo.Pages;
 
 namespace SlideMenuDemo.Views
 {
@@ -12,6 +13,10 @@ namespace SlideMenuDemo.Views
 		{
 			InitializeComponent ();
 		}
+
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			await Navigation.PushAsync(new BBLGitHubWebPage());
+		}
 	}
 }
-
